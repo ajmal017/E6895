ec39c872abd55cc7769c62b81e784457abefdf49
Nandita Mitra <nanda.mitra@gmail.com>
2017-04-10 12:58:56 -0400

    Added 11 stock categories from NASDAQ, removed zero-allocation stocks from output

--- create_portfilo_data.py
+++ create_portfilo_data.py
@@ -1,4 +1,5 @@
 import sqlite3
+import glob
 
 excludelist = ['TRHC','TRVG','XPER','XCRA','WSFS','WPPGY','VRTS','VIVO','URBN','ZBRA','UHAL','UEIC','TZOO', 'TA', 'BIVV']
 
@@ -46,12 +47,31 @@ def populate_data(symbols_company):
 		sql = f.read()
 		c.executescript(sql)
 
-	categories = [(1, 'Technology'), (2,'Energy'), (3, 'Healthcare'), (4, 'Retailer')]
+	#categories = [(1, 'Technology'), (2,'Energy'), (3, 'Healthcare'), (4, 'Retailer')]
+	categories = [(1, 'Basic Industry'), (2,'Capital Goods'), (3, 'Consumer Services'), (4, 'Energy'), (5, 'Finance'), (6, 'Healthcare'), (7, 'Miscellaneous'), (8, 'Nondurables'), (9,'Public Utility'), (10,'Technology'), (11, 'Transportation')]
+
 	c.executemany('INSERT INTO stock_categories VALUES (?,?)', categories)
+
+	symbol_categories = {}
+	map_categories = { 'basicindustry': 1 ,  'capitalgoods':2, 'consumerservices': 3, 'energy':4, 'finance':5, 'healthcare':6, 'misc':7, 'nondurables':8, 'publicutility':9, 'technology':10, 'transportation':11 }
+	files = glob.glob('data/companylist_*.csv')
+	for f in files:
+		cat = f.split('.')[0].split('_')[1]
+		with open(f,'r') as fr:
+			for line in fr:
+				st = line.split(',')[0][1:-1] # strip of quotes
+				if st  == "" or st == "Symbol":
+					continue
+				symbol_categories[st] = map_categories[cat]
+		
 
 	stocks = []
 	for i in range(len(symbols_company)):
-		stocks.append((i, symbols_company[i][0], symbols_company[i][1],1))
+		cat = 7
+		print symbols_company[i][0]
+		if symbols_company[i][0] in symbol_categories:
+			cat = symbol_categories[symbols_company[i][0]]
+		stocks.append((i, symbols_company[i][0], symbols_company[i][1],cat))
 
 	#stocks = [(1,'IBM'),(1,'GOOG'),(1,'MSFT'),(1,'T'),(1,'AAPL'),(1,'AMZN'),(1,'YHOO'),(1,'TSLA'),(1,'INTC'), (4,'WMT')]
 	c.executemany('INSERT INTO system_stocks VALUES (?,?,?,?)', stocks)
@@ -62,7 +82,6 @@ def populate_data(symbols_company):
 	for r in c.execute('SELECT * FROM system_stocks'):
 		print r	
 	
-
 	conn.commit()
 
 	conn.close()
--- nam2159_project.py
+++ nam2159_project.py
@@ -19,8 +19,8 @@ def porfolio():
 		if request.form["investment"].strip() == "" or request.form["expectedreturn"].strip() == "":
 			res["error"] = "Specify all input values"
 		else:
-			res['symbol'] = []
-			res['company'] = []
+			symbols = []
+			companies = []
 			
 			conn = sqlite3.connect('data/portfolio.db')
 			c = conn.cursor() 
@@ -28,12 +28,21 @@ def porfolio():
 			for r in c.execute('''select cat_name, symbol, company_name from stock_categories A, system_stocks B where A.cat_id = B.cat_id'''):
 				#print r[1]
 				#res['symbol'].append(r[1]+" : "+r[2])	
-				res['symbol'].append(r[1])
-				res['company'].append(r[2])
+				symbols.append(r[1])
+				companies.append(r[2])
 			#print res['symbol']
 			#if request.method == 'POST':	
 			#res["allocation"] = portfolio.optimize_portfolio(res['symbol'], float(request.form['investment']), float(request.form['expectedreturn']), startyear, endyear)		
-			res["allocation"] = portfolio.optimize_portfolio_parallel(res['symbol'], float(request.form['investment']), float(request.form['expectedreturn']), startyear, endyear)		
+			allocation = portfolio.optimize_portfolio_parallel(symbols, float(request.form['investment']), float(request.form['expectedreturn']), startyear, endyear)		
+			res['symbol'] = []
+			res['company'] = []
+			res['allocation'] = []
+			for i in range(len(allocation)):
+				if allocation[i] > 0:
+					res["symbol"].append(symbols[i])
+					res["company"].append(companies[i])
+					res["allocation"].append(allocation[i])
+
 			res["investment"] = request.form['investment']
 			res['expectedreturn'] = request.form['expectedreturn']		
 		
