f009c960132ce4af7d9ee29f1fbe9a39e813c2f6
Nandita Mitra <nanda.mitra@gmail.com>
2017-05-11 14:38:57 -0400

    Added function obtain_and_store_future_return

--- predict_stock_price.py
+++ predict_stock_price.py
@@ -56,8 +56,7 @@ def predict_and_store_all_stock_returns(startyear, endyear, futureyears):
 				lastprice = p
 			print predicted_returns			
 
-
-		conn.commit()	
+		conn.commit()
 
 	conn.close()
 
@@ -78,10 +77,17 @@ def predict_and_store_returns(stock_symbol, startyear, endyear, futureyears):
 				continue
 			price_history.insert(0,float(line.split(',')[5]))
 		X = np.array(range(0,len(price_history)))[:,np.newaxis,]
-		y = np.array(price_history)[:,np.newaxis]	
-		
+		y = np.array(price_history)[:,np.newaxis]			
+
+		yr = 2012
+		for p in range(11, len(price_history), 12):
+			print "Historical value for year ",str(yr)," = ", price_history[p]
+			yr += 1			
+	
 		regr = linear_model.LinearRegression()
 		regr.fit(X, y)
+		# The coefficients
+		print('Coefficients: \n', regr.coef_)
 
 		predict_startyr = endyear + 1
 		predicted_returns = []
@@ -100,11 +106,52 @@ def predict_and_store_returns(stock_symbol, startyear, endyear, futureyears):
 		print predicted_returns		
 
 
-		conn.commit()	
+		conn.commit()
+
+#This function is not implmenented. Anyone 	
+def obtain_and_store_future_returns(api_url, stock_symbol, startyear, endyear, futureyears):
+	conn = sqlite3.connect('data/portfolio.db')
+	c = conn.cursor()
+
+	t = (stock_symbol,)
+	c.execute('DELETE FROM predicted_returns where stock_id in (select stock_id from system_stocks where symbol=?)', t)
+	conn.commit()
+
+	print "******", stock_symbol, "******"
+
+	price_history = []
+	#***********************************************************
+	# Implement here the API call for stock_symbol, and parsing of the returned data,
+        # Populate price_history list with the predicted price (one closing price per year) for the futureyears
+	#***********************************************************
+
+	regr = linear_model.LinearRegression()
+	regr.fit(X, y)
+	# The coefficients
+	print('Coefficients: \n', regr.coef_)
+
+	predict_startyr = endyear + 1
+	predicted_returns = []
+	lastprice = X[-1:][0][0]
+	for i in range(futureyears):
+		p = regr.predict(len(X) - 1 + i*12)[0][0]
+		print "Predicted value for year ", str(predict_startyr+i), " = ", p
+		predicted_returns.append(p - lastprice)
+		t = (stock_symbol,)
+		c.execute('SELECT stock_id FROM system_stocks WHERE symbol=?', t)
+		stock_id =  c.fetchone()
+		if stock_id != None:
+			t = (stock_id[0], predict_startyr + i, p - lastprice)	
+			c.execute('insert into predicted_returns values(?,?,?)',t)
+		lastprice = p
+	print predicted_returns		
+
+
+	conn.commit()	
 
 if __name__ == "__main__":
 	startyear=2012
 	endyear=2016
 
 	predict_and_store_all_stock_returns(startyear, endyear, 10)
-	#predict_and_store_returns('GOOG', startyear, endyear, 30)
+	#predict_and_store_returns('AMZN', startyear, endyear, 30)
